9f18b3047866f22247a7f5a217dbdb5b72929107
Rik de Wit <rikdewit1997@gmail.com>
2020-06-06 01:18:23 +0200

    one row of clones animation

--- public/home.js
+++ public/home.js
@@ -1,7 +1,24 @@
 
 
 let title = document.querySelector("#titleBox");
+let clones = document.querySelectorAll(".bgBox");
 
-title.addEventListener("click", (e) =>{
+title.addEventListener("click", (e) => {
     title.classList.toggle("clicked");
+    setTimeout(launch, 500);
 });
+
+async function launch() {
+    for (let i = 0; i < clones.length; i++) {
+
+            setTimeout(() => {
+                if(i ===3){
+                    title.style.visibility = "hidden"
+                }
+                clones[i].style.visibility = "visible";
+
+            }, i * 150);
+        
+    }
+}
+
